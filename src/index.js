e17e11434517d1d539e7c69cdd1620544e78f4e8
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-13 17:30:59 -0700

    Add dummy action, reducer, store, and provider

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/App/App';
+import createStore from './store.js';
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore();
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={ store }>
+      <App />
+    </Provider>
   </BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
