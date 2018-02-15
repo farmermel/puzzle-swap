9d06f93dbe0ca22498927630e36bfbfb8482fed0
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-14 18:01:19 -0700

    Add types to login form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import '../SignUp/SignUp.css';
 
 const Login = () => {
   return (
-    <form>
-      <input type='text' placeholder='name' />
+    <form className='sign-up'>
+      <input type='email' placeholder='email' />
+      <input type='password' placeholder='password' />
+      <button className='sign-up-btn'>Log in</button>
     </form>
   )
 }
