b24f7a547c09aa5b8196ba387e3c373c8dd79d9e
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-14 18:15:17 -0700

    Transition to async await and add try/catch block

--- src/helpers/apiCalls.js
+++ src/helpers/apiCalls.js
@@ -1,31 +1,41 @@
 import { locationKey, sendGridKey } from './apiKeys';
 
 
-export const getGeoLocation = () => {
-  return fetch(`https://www.googleapis.com/geolocation/v1/geolocate?key=${locationKey}`, {
-    body: JSON.stringify({considerIp:'true'}), 
-    headers: {
-      'content-type': 'application/json'
-    },
-    method: 'POST',
-    mode: 'cors'
-  })
-  .then(response => response.json())
-  .then(response => getCities(response))
+export const getGeoLocation = async () => {
+  try { 
+    const initialFetch = await fetch(`https://www.googleapis.com/geolocation/v1/geolocate?key=${locationKey}`, {
+      body: JSON.stringify({considerIp:'true'}), 
+      headers: {
+        'content-type': 'application/json'
+      },
+      method: 'POST',
+      mode: 'cors'
+    })
+    if(initialFetch.status <= 200) {
+      const latLngResponse = await initialFetch.json()
+      return await getCities(latLngResponse)
+    } else {
+      throw new Error('Bad status code');
+    }
+  } catch (error) {
+    throw `failed to retrieve geolocation: ${error}`
+  }
 }
 
-const getCities = (locationResponse) => {
+const getCities = async locationResponse => {
   const { lat, lng } = locationResponse.location
-  return fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${locationKey}`)
-  .then(response => response.json())
-  .then(response => cleanCitiesData(response.results))
+  const initialFetch = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${locationKey}`);
+  const cityResponse = await initialFetch.json();
+  return cleanCitiesData(cityResponse.results);
+
   // .then(response => `${getAddressCity(address_components, 'short')}, ${getAddressState(address_components, 'short')}`)
 }
 
-const cleanCitiesData = (address) => {
+const cleanCitiesData = address => {
   const findType = type => type.types[0] === "locality";
   const location = address.map(obj => obj);
   const placeName = location.filter(findType);
+
   return placeName[0].address_components[0].short_name;
 }
 
