83492cbcaf283957772a75d8c631600305de4f3e
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-14 18:02:27 -0700

    Add types to inputs

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './SignUp';
 
 const SignUp = () => {
   return (
-    <form>
+    <form className='sign-up'>
       <input type='text' placeholder='name' />
+      <input type='email' placeholder='email' />
+      <input type='password' placeholder='password' />
+      <button className='sign-up-btn'>Sign up</button>
     </form>
   )
 }
