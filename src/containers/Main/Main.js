37426c63bb92725e0ad55781b28f30f98c91746c
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-15 20:25:00 -0700

    Add select filter for puzzles to display. Non functional but controlled form and styled"

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -7,6 +7,13 @@ import PuzzleContainer from '../../components/PuzzleContainer/PuzzleContainer';
 import './Main.css';
 
 class Main extends Component {
+  constructor() {
+    super();
+    this.state = {
+      puzzleFilter: 'all'
+    }
+  }
+
   async componentDidMount() {
     try {
       const { addLocation } = this.props;
@@ -17,6 +24,25 @@ class Main extends Component {
     }
   }
 
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  generateSelect = () => {
+    return (
+      <select name='puzzleFilter'
+                    onChange={(e) => this.handleChange(e)}
+                    id='puzzle-filter'>
+        <option value='all'>all</option>
+        <option value='300-750'>300-750</option>
+        <option value='1000'>1000</option>
+        <option value='1100 or more'>1100 or more</option>
+      </select>
+    )
+  }
+
   render() {
     const { location = '' } = this.props;
     return (
@@ -24,7 +50,7 @@ class Main extends Component {
         <Link to={{ pathname: '/post-puzzle-form' }}>
           <button className='puzzle-post'>Post a Puzzle</button>
         </Link>
-        <p>Viewing puzzles posted in {location}</p>
+        <p>Viewing {this.generateSelect()} piece puzzles posted in {location}</p>
         <PuzzleContainer />
       </div>
     ) 
