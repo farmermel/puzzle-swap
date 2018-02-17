95fe2c34093abe9f04ec96a3455313e37b076b46
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-17 12:38:34 -0700

    Add fetch of data from firebase for puzzles

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -6,11 +6,22 @@ export const doCreateUser = (id, username, email) =>
     email
 });
 
-export const onceGetUsers = () => 
-  db.ref('users').once('value');
+// export const onceGetUsers = () => 
+//   db.ref('users').once('value');
+
+export const getOnce = path =>
+  db.ref(path).once('value');
 
 export const getFirebaseKey = childNode => 
   db.ref().child(childNode).push().key;
 
 export const postUpdate = updates => 
   db.ref().update(updates);
+
+export const watchData = watchNode => 
+  db.ref(watchNode);
+
+// export const getData = watchNode => 
+//   watchData(watchNode).on('value', (snapshot) => {
+//     return snapshot.val()
+//   })
