498db1f3fc1925ec76a9e798c6f81a828ac146c2
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-17 12:36:16 -0700

    Temporarily convert puzzle card to have state so that it will update when image is received. Proof of concept, will migrate image storage to store

--- src/components/PuzzleCard/PuzzleCard.js
+++ src/components/PuzzleCard/PuzzleCard.js
@@ -1,15 +1,79 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { store } from '../../firebase';
 import './PuzzleCard.css';
 
-const PuzzleCard = () => {
-  return (
-    <article className='puzzle-card'>
-      <h3>Witch Puzzle</h3>
-      <p>1,000 pieces</p>
-      <p>1-3 pieces missing</p>
-      <button className='claim'>Claim</button>
-    </article>
-  )
+
+const getImg = async (imgId) => {
+  try{
+    const ref = await store.getStoreRef(`images/${imgId}`)
+    const imgUrl = await ref.getDownloadURL();
+    return imgUrl;
+  } catch (error) {
+    console.log(error)
+  }
 }
+
+class PuzzleCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      imgUrl: ''
+    }
+  }
+
+  componentDidMount = async () => {
+    const { puzzle } = this.props;
+    const { puzzleId } = puzzle
+    try{
+      const ref = await store.getStoreRef(`images/${puzzleId}`)
+      const imgUrl = await ref.getDownloadURL();
+      // return imgUrl;
+      this.setState({imgUrl})
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  render() {
+    const { puzzle } = this.props;
+    const styleObj = {
+      backgroundImage: `url(${this.state.imgUrl})`
+    }
+    return (
+      <article className='puzzle-card'>
+        <div className='puzzle-img' style={styleObj}></div>
+        <h3>{puzzle.title}</h3>
+        <p>{puzzle.numPieces}</p>
+        <p>{puzzle.piecesMissing}</p>
+        <button className='claim'>Claim</button>
+      </article>
+    )
+  }
+}
+
+// const getImg = async (imgId) => {
+//   try{
+//     const ref = await store.getStoreRef(`images/${imgId}`)
+//     const imgUrl = await ref.getDownloadURL();
+//     return imgUrl;
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
+// const PuzzleCard = ({ puzzle }) => {
+// console.log(puzzle)
+//   return (
+//     <article className='puzzle-card'>
+//       <img src={getImg(puzzle.puzzleId)} 
+//            alt={`${puzzle.title} puzzle`}
+//            className='puzzle-img' />
+//       <h3>{puzzle.title}</h3>
+//       <p>{puzzle.numPieces}</p>
+//       <p>{puzzle.piecesMissing}</p>
+//       <button className='claim'>Claim</button>
+//     </article>
+//   )
+// }
 
 export default PuzzleCard;
