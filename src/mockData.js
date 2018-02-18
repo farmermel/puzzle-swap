6c9272aae44a0e6dad00cedb29e6e337ac6169de
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-18 10:22:33 -0700

    Add mock data and puzzle container snapshot

---
+++ src/mockData.js
@@ -0,0 +1,23 @@
+export const mockPuzzles = [
+  {
+    title: 'Witch',
+    numPieces: '1000',
+    piecesMissing: '3-4',
+    imgUrl: 'google',
+    puzzleId: 4
+  },
+  {
+    title: 'Fairy',
+    numPieces: '1000',
+    piecesMissing: '3-4',
+    imgUrl: 'yahoo',
+    puzzleId: 45
+  },
+  {
+    title: 'Unicorn',
+    numPieces: '1500',
+    piecesMissing: '3-4',
+    imgUrl: 'aol',
+    puzzleId: 6
+  }
+]
