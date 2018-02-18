f6644b5e4a63a05b9bfb654b78262eed36717784
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-18 11:16:02 -0700

    Add outline for tests and do preliminary tests

--- src/components/PostPuzzleForm/PostPuzzleForm.test.js
+++ src/components/PostPuzzleForm/PostPuzzleForm.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import PostPuzzleForm from './PostPuzzleForm';
+import { PostPuzzleForm } from './PostPuzzleForm';
 
 describe('PostPuzzleForm', () => {
   let wrapper;
@@ -14,12 +14,38 @@ describe('PostPuzzleForm', () => {
   })
 
   it('has default state', () => {
-
+    const expected = {
+      "title": "",
+      "numPieces": "", 
+      "piecesMissing": "1-3", 
+      "error": null 
+    }
+    expect(wrapper.instance().state).toEqual(expected);
   })
 
   describe('handleChange', () => {
     it('sets state with event passed in', () => {
       
     })
+  })
+
+  describe('handlePhoto', () => {
+    it('sets state with a File', () => {
+
+    })
+  })
+
+  describe('postToDB', () => {
+    it('calls getFirebaseKey method of database with pathway \'puzzles\' as argument', () => {
+
+    })
+
+    it('calls postUpdate method of database with an updates object as argument', () => {
+
+    })
+  })
+
+  describe('postToCloudStore', () => {
+    it('calls getStoreRef on ')
   })
 })
