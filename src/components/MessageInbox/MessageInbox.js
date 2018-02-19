1b0a977925eece673de403550f5bcf2188bed74a
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-19 14:13:43 -0700

    Filter existing chats in backend and set current users chats in store. Also display them

--- src/components/MessageInbox/MessageInbox.js
+++ src/components/MessageInbox/MessageInbox.js
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { db } from '../../firebase';
+import { connect } from 'react-redux';
+import { setUsersChats } from '../../actions/userChats';
+import Chat from '../../containers/Chat/Chat';
 
-const MessageInbox = () => {
+export class MessageInbox extends Component {
+  async componentDidMount() {
+    const chatsSnapshot = await db.getOnce('chats');
+    const chats = chatsSnapshot.val();
+    this.setChats(chats);
+  }
+
+  setChats = (chats) => {
+    const { userId, setUsersChats } = this.props;
+    const usersChats = Object.keys(chats).reduce((usersChats, chat) => {
+      chats[chat].members[userId] && usersChats.push(chats[chat])
+      return usersChats;
+    }, [])
+    setUsersChats(usersChats);
+  }
+
+  renderChats = () => {
+    const { usersChats = [] } = this.props;
+    return usersChats.map( chat => (
+      <Chat chat={chat}
+            key={chat.chatId} />
+    ))
+  }
+
   //click of chat goes to associated chat
-  return (
-    <div>
-      <p>chat blurb</p>
-      <p>chat blurb</p>
-      <p>chat blurb</p>
-      <p>chat blurb</p>
-    </div>
-  )
+  render() {
+    return (
+      <div>
+        {this.renderChats()}
+      </div>
+    )
+  }
 }
 
-export default MessageInbox;
+const mapStateToProps = state => ({
+  userId: state.user.uid,
+  usersChats: state.usersChats
+})
+
+const mapDispatchToProps = dispatch => ({
+  setUsersChats: usersChats => dispatch(setUsersChats(usersChats))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(MessageInbox);
