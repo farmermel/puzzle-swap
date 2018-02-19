a39d19fc25adba959882a695d7dce340436d224c
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-19 14:17:43 -0700

    Receive chats from storage as props and render the chat time stamp

--- src/containers/Chat/Chat.js
+++ src/containers/Chat/Chat.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
-const Chat = () => {
+const Chat = ({ chat }) => {
   //on claim, start a new chat between current user and puzzle owner
   //gray out claim buttons for puzzles that are current user's
   //chats have {members: uid: 'name', uid: 'name'}, 'last message', 'timestamp', and 'id' that links
   //to messages array located under messages in storage
+    console.log(chat)
   return (
-    <p>message</p>
-    <p>message</p>
-    <p>message</p>
-    <p>message</p>
-    <p>message</p>
+    <div>
+      <h2>{chat.timeStamp}</h2>
+      <p>I'm a chat blurb</p>
+      <p>hopefully one day I'll also print a bunch of messages conditionally</p>
+    </div>
   )
 }
 
