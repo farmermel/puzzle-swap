55570ed3a390dba50c0fc6f82bdb3c0058cf1cfb
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-20 08:08:18 -0700

    Improve tests outline and write a few tests

--- src/containers/PuzzleContainer/PuzzleContainer.test.js
+++ src/containers/PuzzleContainer/PuzzleContainer.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { db } from '../../firebase';
 import { PuzzleContainer,
 mapStateToProps,
 mapDispatchToProps } from './PuzzleContainer';
@@ -18,30 +19,47 @@ describe('PuzzleContainer', () => {
   })
 
   describe('componentDidMount', () => {
-    it.skip('does some stuff', () => {
-
-    })
-  })
-
-  describe('retrievePuzzles', () => {
-    it.skip('calls watchData on db with whatever is passed', () => {
+    it.skip('calls retrivePuzzles', () => {
 
     })
   })
 
   describe('parseChats', () => {
-    it('returns an existing chat if ownerId and claimerId match an existing chat', () => {
+    const mockChats = {
+      1: {
+        members: {
+          1: 'Im a member',
+          2: 'Im another member'
+        }
+      }
+    }
 
+    it('returns an existing chat if ownerId and claimerId match an existing chat', () => {
+      const ownerId = 1;
+      const claimerId = 2;
+      expect(wrapper.instance().parseChats(mockChats, ownerId, claimerId)).toEqual('1');
     })
 
     it('returns undefined if no chat matches ownderId and claimerId', () => {
-
+      const ownerId = 0;
+      const claimerId = 4;
+      expect(wrapper.instance().parseChats(mockChats, ownerId, claimerId)).toEqual(undefined);
     })
   })
 
   describe('checkForExistingChat', () => {
+    // beforeAll(() => {
+    //   db.getOnce = jest.fn().mockImplementation(() => {
+    //     return { val: () => {return { 1: 'value'}} }
+    //     })
+    //   });
+    //   wrapper.instance().parseChats = jest.fn().mockImplementation(() => {
+    //     return 1
+    //   })
+    // })
+
     it.skip('calls getOnce on db with chats as argument', () => {
-        
+      // expect(wrapper.instance().checkForExistingChat(1, 2)).toEqual()
     })
 
     it.skip('calls parseChats', () => {
@@ -72,24 +90,67 @@ describe('PuzzleContainer', () => {
     })
   })
 
+  describe('getUserNames', () => {
+    beforeAll(() => {
+      db.getOnce = jest.fn().mockImplementation(() => {
+        return { val: () => {
+          return {
+            1: {username: 'mel'},
+            2: {username: 'other user'}
+          }
+        }}
+      })
+    })
+
+    it('calls getOnce on db with users as argument', async () => {
+      expect(db.getOnce).not.toHaveBeenCalled();
+      await wrapper.instance().getUserNames('1', '2');
+      expect(db.getOnce).toHaveBeenCalledWith('users');
+    })
+
+    it('returns an object with ids as keys and names as values', async () => {
+      const expected = {"1": "mel", "2": "other user"};
+      expect(await wrapper.instance().getUserNames('1', '2')).toEqual(expected);
+    })
+  })
+
   describe('makeNewChat', () => {
-    it('calls getFirebaseKey method on db with chats as argument', () => {
+    // beforeAll(() => {
+    //   const mockUsernames = {"1": "mel", "2": "other user"};
+    //   db.getFirebaseKey = jest.fn().mockImplementation(() => {
+    //     return 2;
+    //   })
+
+    //   db.postUpdate = jest.fn();
+
+    //   wrapper.instance().getUserNames = jest.fn().mockImplementation(() => {
+    //     return mockUsernames;
+    //   })
+
+    //   wrapper.instance().checkForExistingChat = jest.fn().mockImplementation(() => {
+    //     return 'done'
+    //   });
+    // })
+
+    it.skip('calls getFirebaseKey method on db with chats as argument', async () => {
+      // expect(db.getFirebaseKey).not.toHaveBeenCalled();
+      // await wrapper.instance().makeNewChat('1', '2');
+      // expect(db.getFirebaseKey).toHaveBeenCalled();
+    })
+
+    it.skip('makes a postDB object with members, a timestamp, a last message, and a chatId', () => {
 
     })
 
-    it('makes a postDB object with members, a timestamp, a last message, and a chatId', () => {
+    it.skip('calls postUpdate on db with an updates object', () => {
 
     })
 
-    it('calls postUpdate on db with an updates object', () => {
+    it.skip('calles checkForExistingChat', () => {
 
     })
 
-    it('calles checkForExistingChat', () => {
-
-    })
-
-    it('catches error if anything fails', () => {
+    it.skip('catches error if anything fails', () => {
 
     })
   })
@@ -104,6 +165,12 @@ describe('PuzzleContainer', () => {
     })
 
     it('calls makeNewChat if there is no existing chat', () => {
+
+    })
+  })
+
+  describe('retrievePuzzles', () => {
+    it('calls watchData on db with whatever is passed', () => {
 
     })
   })
