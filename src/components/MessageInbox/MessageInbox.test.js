b29c44a9d633c09f39d387b8b7a9d6e04e5513f6
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-20 08:03:36 -0700

    Import messageinbox class without connect

--- src/components/MessageInbox/MessageInbox.test.js
+++ src/components/MessageInbox/MessageInbox.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import MessageInbox from './MessageInbox';
+import { MessageInbox, mapStateToProps, mapDispatchToProps } from './MessageInbox';
 
 describe('MessageInbox', () => {
   it('exists', () => {
