deef51e74755a4abe92512a8bd30577ef3c50696
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-21 14:55:36 -0700

    Remove unused notes

--- src/containers/Chat/Chat.js
+++ src/containers/Chat/Chat.js
@@ -9,8 +9,6 @@ const getMembers = members => {
 }
 
 const Chat = ({ chat }) => {
-  //on claim, start a new chat between current user and puzzle owner
-  //gray out claim buttons for puzzles that are current user's
   //chats have {members: uid: 'name', uid: 'name'}, 'last message', 'timestamp', and 'id' that links
   //to messages array located under messages in storage
   return (
