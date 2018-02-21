6fb70b1f8cb1b3a1de5c1259a1edb4e2d4755e6a
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-21 13:05:40 -0700

    Remove chat retrieval from message inbox component

--- src/components/MessageInbox/MessageInbox.js
+++ src/components/MessageInbox/MessageInbox.js
@@ -1,27 +1,12 @@
 import React, { Component } from 'react';
 import { db } from '../../firebase';
 import { connect } from 'react-redux';
-import { setUsersChats } from '../../actions/userChats';
+// import { setUsersChats } from '../../actions/userChats';
 import Chat from '../../containers/Chat/Chat';
 import PropTypes from 'prop-types';
 import './MessageInbox.css';
 
 export class MessageInbox extends Component {
-  async componentDidMount() {
-    const chatsSnapshot = await db.getOnce('chats');
-    const chats = chatsSnapshot.val();
-    this.setChats(chats);
-  }
-
-  setChats = (chats) => {
-    const { userId, setUsersChats } = this.props;
-    const usersChats = Object.keys(chats).reduce((usersChats, chat) => {
-      chats[chat].members[userId] && usersChats.push(chats[chat])
-      return usersChats;
-    }, [])
-    setUsersChats(usersChats);
-  }
-
   renderChats = () => {
     const { usersChats = [] } = this.props;
     return usersChats.map( chat => (
@@ -30,7 +15,6 @@ export class MessageInbox extends Component {
     ))
   }
 
-  //click of chat goes to associated chat
   render() {
     return (
       <div className='inbox'>
@@ -45,9 +29,9 @@ export const mapStateToProps = state => ({
   usersChats: state.usersChats
 })
 
-export const mapDispatchToProps = dispatch => ({
-  setUsersChats: usersChats => dispatch(setUsersChats(usersChats))
-})
+// export const mapDispatchToProps = dispatch => ({
+//   setUsersChats: usersChats => dispatch(setUsersChats(usersChats))
+// })
 
 MessageInbox.propTypes = {
   userId: PropTypes.string,
@@ -60,4 +44,4 @@ MessageInbox.propTypes = {
   setUsersChats: PropTypes.func.isRequired
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(MessageInbox);
+export default connect(mapStateToProps, null)(MessageInbox);
