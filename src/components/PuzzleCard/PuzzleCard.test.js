0278876244d5c7111267ed8bef47c7bf3d378aa7
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-21 12:28:56 -0700

    Write puzzle card tests

--- src/components/PuzzleCard/PuzzleCard.test.js
+++ src/components/PuzzleCard/PuzzleCard.test.js
@@ -4,10 +4,19 @@ import PuzzleCard from './PuzzleCard';
 import { mockPuzzles } from '../../mockData';
 
 describe('PuzzleCard', () => {
-  const mockPuzzle = mockPuzzles[0]
-  it('should exist', () => {
+  const mockPuzzle = mockPuzzles[0];
+
+  it('matches snapshot if user is logged in', () => {
     const wrapper = shallow(<PuzzleCard puzzle={ mockPuzzle }
-                                        handleClaim={ jest.fn() } />);
-    expect(wrapper).toBeDefined();
+                                  handleClaim={ jest.fn() }
+                                  user={{uid: '5', username: 'Nymeria'}} />);
+    expect(wrapper).toMatchSnapshot();
+  })
+
+  it('matches snapshot if user is not logged in', () => {
+    const wrapper = shallow(<PuzzleCard puzzle={ mockPuzzle }
+                                  handleClaim={ jest.fn() }
+                                  user={null} />);
+    expect(wrapper).toMatchSnapshot();
   })
 })
