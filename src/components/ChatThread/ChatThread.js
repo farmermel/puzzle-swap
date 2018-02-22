0f30eadf74b9a11a3c841ee4d62b0a48a33d6fda
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-21 18:48:38 -0700

    Add ability to send messages in a chat thread. Messages post to database under the same id as their associated chat and also update most recent post in chat

--- src/components/ChatThread/ChatThread.js
+++ src/components/ChatThread/ChatThread.js
@@ -1,23 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { db } from '../../firebase';
 import './ChatThread.css';
-
-
-
-//right now this is rendering the same data as a chat
-//should eventually render message threads
 
 class ChatThread extends Component {
   constructor() {
     super();
     this.state = {
-      message: ''
+      message: '',
+      messagesToRender: []
     }
   }
 
+  async componentDidMount() {
+    const { chat } = this.props;
+    const messagesSnap = await db.watchData(`messages/${chat.chatId}`)
+    messagesSnap.on('value', snapshot => {
+      this.renderMessages(snapshot.val())
+    })
+  }
+
   getMembers = members => {
-    return Object.keys(members).map( member => (
+    const membersArr = Object.keys(members).map( member => (
       members[member]
     ))
+    return membersArr.join(', ')
   }
 
   handleChange = (e) => {
@@ -26,13 +33,64 @@ class ChatThread extends Component {
     })
   }
 
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { user, chat } = this.props;
+      const date = Date()
+      const postDB = {
+        username: user.username,
+        uid: user.uid,
+        message: this.state.message,
+        timeStamp: date.toString()
+      }
+      const firebaseKey = db.getFirebaseKey(`messages/${chat.chatId}`);
+      let updates = {};
+      updates[`messages/${chat.chatId}/${firebaseKey}`] = postDB;
+      updates[`chats/${chat.chatId}/lastMessage`] = this.state.message;
+      updates[`chats/${chat.chatId}/timeStamp`] = postDB.timeStamp;
+      this.setState({ message: '' })
+      await db.postUpdate(updates);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  // getMessages = async () => {
+  //   // const { chat } = this.props;
+  //   // const messagesSnap = await db.watchData(`messages/${chat.chatId}`)
+  //   // messagesSnap.on('value', snapshot => {
+  //   //   this.renderMessages(snapshot.val())
+  //   // })
+  // }
+
+  renderMessages = messages => {
+    const messagesToRender = messages
+      ? Object.keys(messages).map( mkey => {
+          return (
+            <article>
+              <div>
+                <p>{messages[mkey].timeStamp}</p>
+                <h3>{messages[mkey].username}</h3>
+              </div>
+              <p>{messages[mkey].message}</p>
+            </article>
+          )
+        })
+      : <h3>Introduce yourself!</h3>
+    this.setState({ messagesToRender })
+  }
+
   render() {
     const { chat } = this.props;
     return (
       <section className='chat-thread'>
-        I'm a chat thread!
-        <h3>Members: <span>{this.getMembers(chat.members)}</span></h3>
-        <form className='chat-form'>
+        <h3 className='chat-members'>Members: <span>{this.getMembers(chat.members)}</span></h3>
+        <section className='messages-wrap'>
+          {this.state.messagesToRender}
+        </section>
+        <form onSubmit={ this.handleSubmit }
+              className='chat-form'>
           <input type='text'
                  name='message'
                  value={ this.state.message }
@@ -44,4 +102,8 @@ class ChatThread extends Component {
   }
 }
 
-export default ChatThread;
+const mapStateToProps = state => ({
+  user: state.user
+})
+
+export default connect(mapStateToProps, null)(ChatThread);
