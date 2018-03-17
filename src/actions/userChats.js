376120a52ccf33e57c91b357e38c79297147a658
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-03-17 09:30:14 -0600

    Add action for adding one user chat to existing chats

--- src/actions/userChats.js
+++ src/actions/userChats.js
@@ -2,3 +2,8 @@ export const setUsersChats = usersChats => ({
   type: 'SET_USERS_CHATS',
   usersChats
 });
+
+export const addOneUserChat = userChat => ({
+  type: 'ADD_ONE_USERCHAT',
+  userChat
+})
