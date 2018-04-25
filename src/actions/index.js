633c7eba544a9889e84cb543278672127d96302f
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-04-25 17:10:49 -0600

    Export all actions from index file

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,10 +2,16 @@ import { addLocation } from './addLocation';
 import { setUser } from './usersActions';
 import { setUsersChats } from './userChats';
 import { hasErrored } from './hasErrored';
+import { setPuzzles } from './setPuzzles';
+import { addOneUserChat } from './userChats';
+import { usersActions } from './usersActions';
 
 export {
   addLocation,
   hasErrored,
   setUser,
-  setUsersChats
+  setUsersChats,
+  setPuzzles,
+  addOneUserChat,
+  usersActions
 };
